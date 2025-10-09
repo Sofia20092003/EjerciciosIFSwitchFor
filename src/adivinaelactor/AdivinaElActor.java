5283035b2ea633b69ca10a5c713847a94de94b10
Sofia20092003 <angelasofiarodriguezarcinegas03@liceolapaz.net>
2025-10-09 10:40:03 +0200

    Ejercicio Adivina el Actor Terminado

--- src/adivinaelactor/AdivinaElActor.java
+++ src/adivinaelactor/AdivinaElActor.java
@@ -12,23 +12,29 @@ public class AdivinaElActor {
         boolean haAdivinado=false;
         System.out.println("Intenta adivinar el actor, tienes tres intentos.");
         System.out.println("------------------------------------------------");
-        System.out.println("Pista: Dos de sus peliculas más famosas en donde " +
+        System.out.println("Pista 1: Dos de sus peliculas más famosas en donde " +
                 "fue el protagonista son: "+pelicula1+ " y " + pelicula2);
         System.out.println("------------------------------------------------");
-        for (int i=3; i>=1; i--){
-            System.out.println("Te quedan "+i+ " intentos");
-            System.out.println("Escribe el nombre del actor que crees que es: ");
+        System.out.println("Tienes 3 intentos");
+        for (int i=1; i<=3; i++){
+            System.out.println("Intento "+i+": ¿Quien es el actor?");
             inentoAdivinar = sc.nextLine();
             if (inentoAdivinar.equalsIgnoreCase(actor)){
                 haAdivinado=true;
-                System.out.println("¡" +haAdivinado+"!"+" ¡Has adivinado el actor!");
+                System.out.println("¡Correcto! ¡Has adivinado! El actor era Harrison Ford.");
                 break;
-            }else {
-                System.out.println("¡"+haAdivinado+"! Intentalo de nuevo.");
-            }
-            System.out.println("------------------------------------------------");
-            if(i==1){
-                System.out.println("Se te han acabado las oportunidades.");
+            } else if (i==1) {
+                System.out.println("¡Incorrecto! Te quedan 2 intentos");
+                System.out.println("------------------------------------------------");
+                System.out.println("Pista 2: Su personaje a menudo lleva un sombrero y un latigo.");
+                System.out.println("------------------------------------------------");
+            } else if (i==2) {
+                System.out.println("¡Incorrecto! Te queda 1 intento");
+                System.out.println("------------------------------------------------");
+                System.out.println("Pista 3: Tambien es famoso por pilotar el Hálcon Milenario en otra saga.");
+                System.out.println("------------------------------------------------");
+            }else if(i==3){
+                System.out.println("¡Incorrecto! Se te han acabado las oportunidades.");
                 break;
             }
         }
