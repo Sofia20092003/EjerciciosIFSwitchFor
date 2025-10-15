147d61318f3502921e470b565666d44d59557ad3
Sofia20092003 <angelasofiarodriguezarcinegas03@liceolapaz.net>
2025-10-15 08:52:26 +0200

    Ejercicio Verificación Velocidad Corregido

--- src/verificadordevelocidad/VerificadordeVelocidad.java
+++ src/verificadordevelocidad/VerificadordeVelocidad.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 public class VerificadordeVelocidad {
     public static void main(String[] args) {
         int velocidadcoche;
-        boolean esexcesovelocidadv=true;
-        boolean esexcesovelocidadf=false;
+        boolean esexcesovelocidadf;
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite la velocidad actual de su vehiculo(km/h):");
         velocidadcoche = sc.nextInt();
         if (velocidadcoche>120) {
-            System.out.println(esexcesovelocidadv);
+            esexcesovelocidadf=true;
             System.out.println("¡Atención! Vas en una velocidad excesiva. ¡Riesgo de Multa!");
         }else  {
-            System.out.println(esexcesovelocidadf);
+            esexcesovelocidadf=false;
             System.out.println("Velocidad adecuada. ¡Sigue asi y conduce con seguridad!");
         }
     }
